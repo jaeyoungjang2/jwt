3f10ae15d70ddc4df1799047740d9767ae14e64c
jaeyoungjang2 <lion85488@gmail.com>
2022-10-16 16:25:00 +0900

    feat: 게시글 전체 조회

--- src/main/java/com/study/jwt/controller/Postcontroller.java
+++ src/main/java/com/study/jwt/controller/Postcontroller.java
@@ -6,6 +6,8 @@ import com.study.jwt.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
@@ -13,14 +15,21 @@ public class Postcontroller {
 
     private final PostService postService;
 
+    // 게시글 저장
     @PostMapping("")
     private PostResponseDto savePost(PostRequestDto postRequestDto) {
         return postService.savePost(postRequestDto);
     }
 
+    // 게시글 단건 조회
     @GetMapping("/{id}")
     private PostResponseDto findPost(@PathVariable Long id) {
         return postService.findPost(id);
     }
 
+    // 게시글 전체 조회
+    @GetMapping("")
+    private List<PostResponseDto> findAllPost() {
+        return postService.findAllPost();
+    }
 }
--- src/main/java/com/study/jwt/post/service/PostService.java
+++ src/main/java/com/study/jwt/post/service/PostService.java
@@ -6,6 +6,9 @@ import com.study.jwt.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -25,5 +28,16 @@ public class PostService {
         );
 
         return new PostResponseDto(post);
+    }
+
+    public List<PostResponseDto> findAllPost() {
+        List<Post> foundPosts = postRepository.findAll();
+
+        List<PostResponseDto> postResponseDtos = new ArrayList<>();
+        for (Post foundPost : foundPosts) {
+            postResponseDtos.add(new PostResponseDto(foundPost));
+        }
+
+        return postResponseDtos;
     }
 }
